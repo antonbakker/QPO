15257684381c2842e31b8a81c6f9d8026f7c8204
Anton <anton@toontje2016.local>
2017-08-01 12:21:12 +0200

    Query aangepast

--- Uren/Controle uren import.sql
+++ Uren/Controle uren import.sql
@@ -1,74 +1,92 @@
 set transaction isolation level read uncommitted
-SELECT DISTINCT  Qurencontrole.Urenid,Referentie, Aanmaakdatum, ProjectNr
+SELECT DISTINCT
+  Qurencontrole.Urenid AS UrenUD,
+  Qurencontrole.Referentie AS Referentie,
+  Qurencontrole.Aanmaakdatum AS Aanmaakdatum,
+  Qurencontrole.ProjectNr AS ProjectNr
 
 FROM (
-SELECT freefield5
+SELECT
+  freefield5
 FROM [512].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [521].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [522].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [523].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [524].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [525].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [527].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [528].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [541].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [561].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 UNION ALL
 
-SELECT freefield5
+SELECT
+  freefield5
 FROM [547].dbo.gbkmut
 where freefield5 is not null and dagbknr=' 98'
 
 ) AS alleurenids RIGHT OUTER JOIN
   Qurencontrole ON alleurenids.freefield5 = Qurencontrole.Urenid
-WHERE (alleurenids.freefield5 IS NULL) and Referentie is not null
-and  left (Projectnr,2) <> '43' 
 
-order by 4
+WHERE
+  (alleurenids.freefield5 IS NULL)
+  -- and Referentie is not null
+  -- and  left (Projectnr,2) <> '43' 
+
+order by Qurencontrole.Aanmaakdatum DESC
